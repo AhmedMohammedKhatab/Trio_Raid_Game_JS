cc872fbd322ceeb2ee3ea784df77f7558d3f2f1d
amrmousa95 <amrmousa95@gmail.com>
2019-10-15 15:27:23 -0700

    Last Modification of Project version 2.0.5

--- assets/js/main.js
+++ assets/js/main.js
@@ -472,3 +472,10 @@ function levels(speed, srcHero, srcEnemy, shotHeroSpeed) {
     };
 }
 
+// var begin = document.getElementById('start');
+// if(begin != null){
+//     begin.addEventListener('click', function () {
+//         var audio = new Audio('../assets/audio/bg.mp3');
+//         audio.play();
+//     });
+// }
